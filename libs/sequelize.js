d1a13e6b8c6f74b46e3ee5730e777662d761c0b0
Leonardo David Flores Sarango <93964490+leofru2002@users.noreply.github.com>
2025-03-06 19:22:13 -0500

    Update sequelize.js

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,15 +1,20 @@
-const { Sequelize } = require('sequelize');
-const { config } = require('./../config/config');
-const initModels = require('./../db/models');
-const USER = encodeURIComponent(config.dbUser);
-const PASS = encodeURIComponent(config.dbPass);
-const URI = `mysql://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
-  logging: (msg) => console.log(msg),
-});
+require('dotenv').config(); // Asegura que las variables de entorno están cargadas
+const config = require('../config/config'); // Importa el archivo de configuración
 
-initModels(sequelize);
-/* sequelize.sync(); */
+const { Sequelize } = require('sequelize');
+
+const USER = encodeURIComponent(config.production.username);
+const PASSWORD = encodeURIComponent(config.production.password);
+const DB_NAME = config.production.database;
+const DB_HOST = config.production.host;
+const DB_PORT = config.production.port;
+const DIALECT = 'mysql';
+
+const URI = `mysql://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const sequelize = new Sequelize(URI, {
+  dialect: DIALECT,
+  logging: false,
+});
 
 module.exports = sequelize;
