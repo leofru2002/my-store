f8badb91bf17eda58275f4cbd851b06cac8919a0
Leonardo David Flores Sarango <93964490+leofru2002@users.noreply.github.com>
2025-03-06 19:03:44 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,4 +9,21 @@ const config = {
   dbPort: process.env.DB_PORT,
 };
 
-module.exports = { config };
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  },
+};
