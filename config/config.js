e9d092dc067dccba742df5522af39aecaf0d90c9
Leonardo David Flores Sarango <93964490+leofru2002@users.noreply.github.com>
2025-03-06 19:22:39 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    database: process.env.DB_NAME || 'myStore',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '3307',
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQLUSER,
+    username: process.env.MYSQLUSER, 
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     host: process.env.MYSQLHOST,
